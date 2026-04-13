0863187f1cb63a215ee4925ed6e31aad04364310
Derek Belrose <derek@derekbelrose.com>
2026-04-13 13:47:10 -0400

    Bootstrap NixOS baseline with disko
    Set up a modular flake with a Proxmox VM host, BIOS-friendly disko partitioning, and shared modules so physical hosts can be added without inheriting VM-specific settings.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,22 @@
 {
-  description = "NixOS project flake";
+  description = "NixOS fleet flake with disko";
 
-  outputs = { self }: {
-    # TODO: add nixosConfigurations, packages, or devShells
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    disko.url = "github:nix-community/disko";
+
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, disko, ... }: {
+    nixosConfigurations = {
+      proxmox-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./hosts/proxmox-vm/default.nix
+        ];
+      };
+    };
   };
 }
---
+++ modules/base.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, ... }:
+{
+  networking.useDHCP = lib.mkDefault true;
+
+  time.timeZone = lib.mkDefault "UTC";
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+
+  system.stateVersion = "25.05";
+}
